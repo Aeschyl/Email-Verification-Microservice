0efae79864471f49a1cdaf14f4ccc719b225c3d4
Aryamann <aryamannsheoran@gmail.com>
2025-01-08 01:00:49 -0500

    removed excess

--- src/main/java/emailverification/service/DisposableEmailChecker.java
+++ src/main/java/emailverification/service/DisposableEmailChecker.java
@@ -6,36 +6,11 @@ import org.springframework.stereotype.Service;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.HashSet;
 import java.util.Set;
 
 @Service
 public class DisposableEmailChecker {
-
-    /*
-     * @Cacheable("domains")
-     * public Set<String> getDisposableDomains() {
-     * Set<String> domains = new HashSet<>();
-     * try (BufferedReader reader =
-     * Files.newBufferedReader(Paths.get("src/main/resources/disposable_domains.txt"
-     * ))) {
-     * String line;
-     * while ((line = reader.readLine()) != null) {
-     * String domain = line.trim().toLowerCase();
-     * if (!domain.isEmpty()) {
-     * domains.add(domain);
-     * }
-     * }
-     * } catch (IOException e) {
-     * System.err.println("Unable to get typical disposable domain names: " +
-     * e.getMessage());
-     * }
-     * return domains;
-     * }
-     * 
-     */
 
     @Cacheable("domains")
     private Set<String> loadDisposableDomains(String filePath) {
